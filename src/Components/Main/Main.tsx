10dacc05b338642479d429667c344ad669039f4c
Isaac Rios <jirb1213@live.com.mx>
2024-07-21 18:42:17 -0600

    main body structure

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,5 +1,14 @@
+import HighLights from "../Highlights/Highlights";
+import Styles from "./Main.module.scss";
 const Main = () => {
-  return <div>main</div>;
+  return (
+    <div className={Styles.main}>
+      <section className="hero"></section>
+      <HighLights />
+      <section className="testimonials"></section>
+      <section className="about"></section>
+    </div>
+  );
 };
 
 export default Main;

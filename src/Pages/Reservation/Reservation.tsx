54d929baa84116e3cbe002f96f34daac1667b74b
Isaac Rios <jirb1213@live.com.mx>
2024-08-18 19:05:38 -0600

    reservation screen title added

--- src/Pages/Reservation/Reservation.tsx
+++ src/Pages/Reservation/Reservation.tsx
@@ -4,10 +4,15 @@ import Styles from "./Reservation.module.scss";
 import ReserveTable from "../../Components/SidePanel/ReserveATable";
 
 const Reservation = () => {
-  // @Dev - fill the next functions
   return (
     <div className={Styles.reservationWrapper}>
-      <ReserveTable onCancel={() => {}} onSave={() => {}} />
+      <h2 className={Styles.reservationTitle}>Reserve a table</h2>
+
+      <ReserveTable
+        onCancel={() => {}}
+        onSave={() => {}}
+        className={Styles.form}
+      />
     </div>
   );
 };
